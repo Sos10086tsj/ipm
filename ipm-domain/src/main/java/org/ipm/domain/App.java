27ba71b90e739adbd6d7bd05939e4e67282dd8d7
Paris Tao <paristao1989@gmail.com>
2014-12-01 17:36:08 +0800

    project initialize
    create parent and sub-modules

---
+++ ipm-domain/src/main/java/org/ipm/domain/App.java
@@ -0,0 +1,13 @@
+package org.ipm.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
