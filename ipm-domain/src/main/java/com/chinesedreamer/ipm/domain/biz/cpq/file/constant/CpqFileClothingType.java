e824b876a185b0566feb533e0eead984f465e446
Pairs Tao <paristao1989@gmail.com>
2015-08-31 19:28:38 +0800

    增加亲子装设置

--- ipm-domain/src/main/java/com/chinesedreamer/ipm/domain/biz/cpq/file/constant/CpqFileClothingType.java
+++ ipm-domain/src/main/java/com/chinesedreamer/ipm/domain/biz/cpq/file/constant/CpqFileClothingType.java
@@ -2,5 +2,5 @@ package com.chinesedreamer.ipm.domain.biz.cpq.file.constant;
 
 
 public enum CpqFileClothingType {
-	DEAFULT,MALE,FEMALE,BOY,GIRL;
+	DEAFULT,MALE,FEMALE,BOY,GIRL,FAMILY;
 }
---
+++ ipm-web/src/main/document/cpq-2015831.sql
@@ -0,0 +1,79 @@
+-- 颜色配置
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '32','32',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '32' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '57','57',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '57' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '62','62',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '62' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '970','970',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '970' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '59','59',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '59' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '01','01',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '01' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'A','A',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'A' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'B','B',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'B' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '43','43',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '43' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '57','57',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '57' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '470','470',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '470' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '90','90',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '90' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'F','F',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'F' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'D','D',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'D' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'E','E',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'E' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '03','03',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '03' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '11','11',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '11' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '87','87',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '87' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '73','73',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '73' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'F','F',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'F' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '72','72',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '72' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'A','A',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'A' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '050','050',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '050' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '51','51',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '51' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '37','37',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '37' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '43','43',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '43' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '66','66',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '66' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '97','97',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '97' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'B','B',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'B' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select 'C','C',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'C' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '570','570',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '570' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '48','48',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '48' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '01','01',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '01' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '62','62',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '62' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '57','57',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '57' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '050','050',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '050' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '360','360',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '360' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '590','590',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '590' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '660','660',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '660' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '050','050',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '050' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '11','11',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '11' and type = 1);
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) select '58','58',1 from dual where not exists (select 1 from ipm.ipm_biz_cpq_data_dictionary where property = '58' and type = 1);
+
+--亲子装配置
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type) 
+select 'FAMILY','亲子装',2 from dual where not exists (
+select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'FAMILY' and type = 2);
+
+
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type,seq) 
+select 'FAMILY','Size UNI-1',3,1 from dual where not exists (
+select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'FAMILY' 
+and type = 3 and value='Size UNI-1');
+
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type,seq) 
+select 'FAMILY','Size UNI-2',3,2 from dual where not exists (
+select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'FAMILY' 
+and type = 3 and value='Size UNI-2');
+
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type,seq) 
+select 'FAMILY','Size UNI-3',3,3 from dual where not exists (
+select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'FAMILY' 
+and type = 3 and value='Size UNI-3');
+
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type,seq) 
+select 'FAMILY','Size UNI-4',3,4 from dual where not exists (
+select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'FAMILY' 
+and type = 3 and value='Size UNI-4');
+
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type,seq) 
+select 'FAMILY','Size UNI-5',3,5 from dual where not exists (
+select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'FAMILY' 
+and type = 3 and value='Size UNI-5');
+
+insert into ipm.ipm_biz_cpq_data_dictionary(property,value,type,seq) 
+select 'FAMILY','Size UNI-6',3,6 from dual where not exists (
+select 1 from ipm.ipm_biz_cpq_data_dictionary where property = 'FAMILY' 
+and type = 3 and value='Size UNI-6');
